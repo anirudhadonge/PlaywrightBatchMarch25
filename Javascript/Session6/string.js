838b6ad3e3992e86e1ae36ee91ba2af1c5c61342
anirudha <anirudhadonge@hotmail.com>
2025-03-25 20:06:28 +0530

    Session 6

---
+++ Javascript/Session6/string.js
@@ -0,0 +1,59 @@
+/**
+ * string is a collection of charaters. 
+ * length : Total number charaters in a string
+ */
+
+//let mystring = 'this is my string';
+// console.log(mystring.length);
+// let mySecondString = "This is my Second String";
+// console.log(typeof(mySecondString));
+
+// for(let i =0 ;i<mystring.length;i++){
+//     console.log(mystring[i]);
+// }
+
+// let val1 = "Anirudha";
+
+// let val2 = 7
+
+// let mystring = 'my name is '+val1+' and my roll Number is '+val2;
+// let mysecondString = `my name is ${val1} and my roll Number is ${val2}`;
+// console.log(mystring);
+// console.log(mysecondString);
+
+
+/**
+ * lenght
+ * charAt()
+ * substring()
+ * toUpperCase()
+ * toLowerCase()
+ * concat()
+ * split()
+ * replace();
+ * replaceALL();
+ * trim();
+ * trimStart();
+ * trimEnd();
+ */
+
+
+// for(let i =0 ;i<mystring.length;i++){
+//     console.log(mystring.charAt(i));
+// }
+// console.log(mystring.toUpperCase());
+// console.log(mystring.toLowerCase());
+
+//console.log(mystring.substring(11,21))
+// let myStringArray=['one string','two string','three string']
+// console.log(mystring.concat(' one more string',myStringArray[1]))
+// console.log(mystring);
+
+// mystring  = mystring.replaceAll('9','and')
+// console.log(mystring);
+// console.log(mystring);
+
+let mystring = " This is 9 my 8 Javascript 9 class ";
+
+console.log("5"+mystring.trim()+"6");
+console.log("5"+mystring+"6");
