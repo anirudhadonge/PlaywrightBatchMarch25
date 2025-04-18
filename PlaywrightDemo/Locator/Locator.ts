4ce63ed6ca9416562658ec4b050886effe3a5cbf
anirudha <10610321+anirudhadonge@users.noreply.github.com>
2025-04-18 07:03:55 +0530

    Session 17-Apr

--- PlaywrightDemo/Locator/Locator.ts
+++ PlaywrightDemo/Locator/Locator.ts
@@ -39,3 +39,13 @@
  * Sibing to immedate sibling
  * 
  */
+
+
+// ○ Locate by role
+// ○ Locate by label
+// ○ Locate by placeholder
+// ○ Locate by text
+// ○ Locate by alt text
+// ○ Locate by title
+// ○ Locate by test id
+// Locate by CSS or XPath
--- PlaywrightDemo/tests/NavigateToInstallationPage.spec.ts
+++ PlaywrightDemo/tests/NavigateToInstallationPage.spec.ts
@@ -3,8 +3,25 @@ import {test,expect} from '@playwright/test'
 
 test("Navigation to Installation Page",async({page})=>{
     await page.goto('https://playwright.dev/');
-    let getStarted = await page.locator(".getStarted_Sjon");
+    let getStarted = await page.locator("");
     getStarted.screenshot();
     getStarted.click();
     await expect(page.locator(".theme-doc-markdown.markdown h1")).toHaveText("Installation");
+})
+
+test.only("Example for GetByRole method",async({page})=>{
+    await page.goto("https://selectorshub.com/xpath-practice-page/");
+    //await page.locator('[href="/checkboxes"]').click();
+    //await expect(page.getByLabel("Password")).toBeVisible();
+    // await page.getByPlaceholder("Enter your company").nth(0).fill("Tata Motors");
+    // //await page.getByRole('button').nth(0).click();
+    console.log("Before Clicking")
+    await page.getByText("Submit").nth(0).click();
+    console.log("After click Clicking")
+    await page.pause();
+    //await page.getByTitle("Enter your first crush name").fill("example");
+
+    //await page.getByTestId()
+
+    //await page.pause();
 })
