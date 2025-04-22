86b4547c6d4dfcbedc4dc02450b52b72afc1693c
anirudha <10610321+anirudhadonge@users.noreply.github.com>
2025-04-22 07:02:10 +0530

    Session for Playwright actions

--- PlaywrightDemo/tests/NavigateToInstallationPage.spec.ts
+++ PlaywrightDemo/tests/NavigateToInstallationPage.spec.ts
@@ -9,12 +9,12 @@ test("Navigation to Installation Page",async({page})=>{
     await expect(page.locator(".theme-doc-markdown.markdown h1")).toHaveText("Installation");
 })
 
-test.only("Example for GetByRole method",async({page})=>{
+test("Example for GetByRole method",async({page})=>{
     await page.goto("https://selectorshub.com/xpath-practice-page/");
     //await page.locator('[href="/checkboxes"]').click();
     //await expect(page.getByLabel("Password")).toBeVisible();
     // await page.getByPlaceholder("Enter your company").nth(0).fill("Tata Motors");
-    // //await page.getByRole('button').nth(0).click();
+    await page.getByRole('button').nth(0).click();
     console.log("Before Clicking")
     await page.getByText("Submit").nth(0).click();
     console.log("After click Clicking")
@@ -24,4 +24,65 @@ test.only("Example for GetByRole method",async({page})=>{
     //await page.getByTestId()
 
     //await page.pause();
+})
+/**
+ * Fill();
+ * Presequentially();
+ */
+
+
+test('Interacting with Input fields',async({page})=>{
+
+    let text = `This is where you can log into the secure area. 
+    Enter tomsmith for the username and SuperSecretPassword! for the password. 
+    If the information is wrong you should see error messages.`
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.getByText('Form Authentication').click();
+    let userNameField = page.locator("#username");
+    let passwordField = page.locator("#password");
+    await page.locator("#username").pressSequentially("tomsmith",{delay:1000});
+    await page.locator("#password").fill(text)
+})
+//selectOptions
+// using the Value Attribue
+// using label
+// using Index 
+test('Interaction with Select tags',async({page})=>{
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.getByText('Dropdown').click();
+    await page.locator("#dropdown").selectOption({value:'1'});
+    await page.locator("#dropdown").selectOption({label:"Option 2"})
+    await page.locator("#dropdown").selectOption({index:1})
+})
+
+test('Interacting with Checkboxes or radio button',async({page})=>{
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.getByText('Checkboxes').click();
+    //await page.locator("#checkboxes input").nth(0).check();
+    if(!await page.locator("#checkboxes input").nth(0).isChecked()){
+        await page.locator("#checkboxes input").nth(0).click();
+    }
+})
+
+
+test('Uploading files',async({page})=>{
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.getByText('File Upload').click()
+    const fileChoose = page.waitForEvent('filechooser');
+    //await page.locator("#file-upload").setInputFiles("./UploadFiles/1000_F_561609331_cmOdHBvlRGhWqPZuB1QBLLlc6nQ1m1eq.jpg");
+    await page.locator("#file-upload").click();
+    const fileCs = await fileChoose;
+    fileCs.setFiles("C:\\UploadFiles\\1000_F_561609331_cmOdHBvlRGhWqPZuB1QBLLlc6nQ1m1eq.jpg");
+});
+
+test.only('Interacting with Iframe/Frames',async({page})=>{
+    await page.goto("https://the-internet.herokuapp.com/");
+    await page.locator('[href="/frames"]').click();
+    await page.locator('[href="/nested_frames"]').click();
+    const topFrame = page.frameLocator('[src="/frame_top"]');
+    const leftTopFrame = topFrame.frameLocator('[src="/frame_left"]')
+    await expect(leftTopFrame.locator('body')).toContainText('LEFT');
+    //await page.locator('[href="/iframe"]').click();
+    // const frameLocator = page.frameLocator('#mce_0_ifr');
+    // await expect(frameLocator.locator('#tinymce p')).toHaveText('Your content goes here.');
 })
